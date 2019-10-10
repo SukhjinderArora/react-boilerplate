00950c24c868cac6470017ce4ed23b3d24cb62fc
Ernestovich <erikfenriz@gmail.com>
2019-10-10 18:34:51 +0300

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -9,10 +9,13 @@ module.exports = {
   },
   module: {
     rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: ["babel-loader"]
+        {
+                test: /\.(js|jsx)$/,
+                resolve: {extensions: [".js", ".jsx"]},
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader"
+                }
       },
       {
         test: /\.css$/,
